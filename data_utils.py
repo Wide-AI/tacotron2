bfc082b178bc539261ba52ef34924f7f8364c8cb
vateseif <vateseif@gmail.com>
2022-11-07 02:10:48 +0100

    minor

--- data_utils.py
+++ data_utils.py
@@ -4,7 +4,7 @@ import torch
 import torch.utils.data
 
 import layers
-from utils import load_wav_to_torch, load_filepaths_and_text
+from utils_tacotron import load_wav_to_torch, load_filepaths_and_text
 from text import text_to_sequence
 
 
